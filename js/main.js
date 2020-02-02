a2e9e67fe578a4ee5e89ea30a717bd3c544c7f24
felipebatista24680@gmail.com <felipebatista24680@gmail.com>
2020-02-02 13:56:22 -0300

    teste-finalizado

--- js/main.js
+++ js/main.js
@@ -2,39 +2,13 @@ $('.carousel').slick({
 
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
     dots: true,
     infinite: true,
 });
+$('.carousel-web').slick({
 
-$('.knowledge').slick({
-    centerMode: true,
-    centerPadding: '10px',
     slidesToShow: 3,
     slidesToScroll: 1,
-
-    // autoplay: true,
-    // autoplaySpeed: 100,
-    // dots: true,
-    // infinite: true,
-    responsive: [{
-            breakpoint: 768,
-            settings: {
-                arrows: true,
-                centerMode: true,
-                centerPadding: '200px',
-                slidesToShow: 3
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                arrows: true,
-                centerMode: true,
-                centerPadding: '400px',
-                slidesToShow: 1
-            }
-        }
-    ]
+    dots: true,
+    infinite: true,
 });
